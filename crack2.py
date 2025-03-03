d3692897f536834d0a00e1b4edddc4487575935e
ri285 <146011347+ri285@users.noreply.github.com>
2025-03-03 13:58:28 +0530

    simplified examples

--- crack2.py
+++ crack2.py
@@ -3,6 +3,6 @@ def custom_decrypt(encrypted_text):
     reversed_text = "".join(chr(ord(c) - shift) for c in encrypted_text)  # Reverse shift
     original_text = reversed_text[::-1]  # Reverse back to original
     print(original_text)
-
+ 
 hidden_encrypted_pass = ""  
 custom_decrypt(hidden_encrypted_pass)
